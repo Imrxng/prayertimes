9f32ff29fa1c5f7889c6f573222224195b12dccb
Imrxng <148759783+Imrxng@users.noreply.github.com>
2024-01-10 16:14:28 +0100

    Update gebedstijden.js

--- js/gebedstijden.js
+++ js/gebedstijden.js
@@ -7,7 +7,8 @@ prayerTimesPicture.src = `./assets/images/${month}.webp`;
 document.getElementById('submitGebedstijden').addEventListener('click', function (e) {
     e.preventDefault();
     let monthSelected = document.getElementById('maanden').value;
-    prayerTimesPicture.src = `./assets/images/${monthSelected}.webp`;
+    if (monthSelected != "") {
+        prayerTimesPicture.src = `./assets/images/${monthSelected}.webp`;
+    }
 });
 
-/*https://alquran.cloud/api */
