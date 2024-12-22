5d8dfa91969819f638ca9e353124da5694ef6ec6
Imrxng <148759783+Imrxng@users.noreply.github.com>
2024-12-22 17:33:30 +0100

    Add files via upload

--- js/gebedstijden.js
+++ js/gebedstijden.js
@@ -1,13 +1,13 @@
 let currentDate = new Date();
 let month = currentDate.getMonth() + 1;
 let prayerTimesPicture = document.getElementById("gebedstijdenFoto");
-prayerTimesPicture.src = `./assets/images/${month}.webp`;
+prayerTimesPicture.src = `./assets/images/${month}.png`;
 
 
 document.getElementById('submitGebedstijden').addEventListener('click', function (e) {
     e.preventDefault();
     let monthSelected = document.getElementById('maanden').value;
     if (monthSelected != "") {
-        prayerTimesPicture.src = `./assets/images/${monthSelected}.webp`;
+        prayerTimesPicture.src = `./assets/images/${monthSelected}.png`;
     }
 });
